2e55e8fbcc65277238c4fe286c420f4edf4f4a64
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-16 13:02:33 +0500

    html, css,php combined

---
+++ php-html.php
@@ -0,0 +1,15 @@
+
+<?php 
+
+echo("<strong>This is strong</strong><br />");
+echo("<i>This is italic</i><br />");
+echo("<h1>This is h1</h1><br/>");
+echo("<h3 style='color:red;'>This is h3</h3>");
+
+$color = "blue"
+
+?>
+
+<p style="color:<?php echo$color?>">
+Helloo I am Blue 
+</p>
