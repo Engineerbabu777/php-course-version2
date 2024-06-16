af6e8780d683d4ea4fa424d0b543e1a551636431
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-16 12:47:56 +0500

    variables, cvreating strings, number, booleanas

---
+++ variables.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$name = "Awais";
+$unmarried = true;
+
+
+
+echo("Your name is ".$name);
+
+echo("<br />");
+
+
+echo($name." is bad boy!");
+echo("<br />");
+
+
+echo("<h1>".$name."</h1> is unmarried ? <h3>Ans:</h3> ".$unmarried)
+
+
+
+?>
