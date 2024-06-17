fc2b31685546bf43c6cfc5fd75599e94ff94ecbd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-17 12:22:35 +0500

    add: data types

---
+++ dat-types.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// all data types!
+
+$int = 10;
+$float = 10.5;
+$string = "Hello";
+$boolean = true;
+$array = array("apple", "banana", "cherry");
+//$object = new stdClass();
+$null = null;
+//$resource = fopen("file.txt", "r");
+$class = "MyClass";
+//$instance = new MyClass();
+$callable = "myFunction";
+//$iterable = new MyClass();
+//$generator = new MyClass();
+
+echo var_dump($int);
+echo var_dump($float);
+echo var_dump($string);
+echo var_dump($boolean);
+echo var_dump($array);
+//echo var_dump($object);
+echo var_dump($null);
+//echo var_dump($resource);
+echo var_dump($class);
+//echo var_dump($instance);
+echo var_dump($callable);
+//echo var_dump($iterable);
+//echo var_dump($generator);
+
+
+?>
